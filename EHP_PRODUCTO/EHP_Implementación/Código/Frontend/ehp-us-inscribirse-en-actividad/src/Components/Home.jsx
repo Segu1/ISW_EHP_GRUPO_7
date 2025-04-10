fac2008da9d2bb6c323dc062dcf3b11633f1ed14
Segu1 <114274560+Segu1@users.noreply.github.com>
2025-04-10 19:05:59 -0300

    cambios de interfaz

--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Components/Home.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Components/Home.jsx
@@ -1,13 +1,29 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+
+    const handleHover = () => {
+      const btnActividades = document.getElementById('btnActividades');
+      btnActividades.textContent = "Inscribirme!"
+    };
+  
+    const handleLeave = () => {
+      const btnActividades = document.getElementById('btnActividades');
+      btnActividades.textContent = "Actividades"
+    };
+
   return (
     <>
-      <body>
+      <body className="">
       <div className="mx-auto justify-center items-center pt-20">
-      <img src="./homeTreeImg.png" alt="Home tree img" className="text-center shadow-2xl p-10 rounded-lg h-120 mx-auto"  />
-      <button class="mx-auto block bg-green-800 text-white px-10 py-2 mt-10 rounded hover:shadow-4xl hover:bg-green-500">Actividades</button>
-
-</div>
-        </body>
+      <img src="./homeTreeImg.png" alt="Home tree img" className="text-center shadow-2xl p-10 rounded-2xl h-120 mx-auto bg-[#042A2B]"  />
+      <button    
+      id="btnActividades"
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}
+      className="mx-auto block bg-green-800 text-white px-10 py-2 mt-10 rounded hover:shadow-4xl hover:bg-green-500">Actividades</button>
+      </div>
+       </body>
     </>
   );
 }
