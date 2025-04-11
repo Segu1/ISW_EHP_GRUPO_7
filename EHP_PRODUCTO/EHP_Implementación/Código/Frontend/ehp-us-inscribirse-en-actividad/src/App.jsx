664ea6fdc42b2494fda370bc4d21aebffe2483ba
Fede-Coder <federiconieto04@gmail.com>
2025-04-10 22:00:27 -0300

    Arreglado los imports

--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/App.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home"
+import Home from "./Views/Home"
 import Actividades from "./Components/Actividades";
 
 function App() {
--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Home.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Home.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "../Components/Navbar";
 
 
 function Home() {
