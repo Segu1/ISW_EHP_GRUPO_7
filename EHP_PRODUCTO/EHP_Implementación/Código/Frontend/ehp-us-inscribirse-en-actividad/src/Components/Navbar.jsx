5934e6e58b7c283dd811be46c1a1bd97eeda550c
Segu1 <114274560+Segu1@users.noreply.github.com>
2025-04-11 14:43:07 -0300

    diseño responsive de actividades, faltan

--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Components/Navbar.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@
 function Navbar(props){
     return(
         <>
-        <div className="grid font-mono text-shadow-lg items-center text-white w-full justify-center text-center p-3 rounded-sm bg-[#1F363D]">
+        <div className="grid mb-5 text-shadow-lg items-center text-white w-full justify-center text-center p-3 rounded-sm bg-[#1F363D]">
             <p className="mt-1 block">EcoHarmony Park</p>
             <p> {props.site}</p>
         </div>
