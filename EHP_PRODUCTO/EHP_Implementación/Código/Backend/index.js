4d3e0c90f93d8dc2a55f5b05bfcf664a633038d4
Lucas <67369@sistemas.frc.utn.edu.ar>
2025-04-12 10:58:53 -0300

    Route actividades

--- EHP_PRODUCTO/EHP_Implementación/Código/Backend/index.js
+++ EHP_PRODUCTO/EHP_Implementación/Código/Backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { config } from "dotenv";
 import { sequelize } from "./database/database.js";
 import { GestorActividades } from "./controllers/actividades_controller.js";
+import { routerActividades } from "./routes/actividades_routes.js";
 
 const app = express();
 const port = process.env.PORT || 5050;
@@ -17,6 +18,9 @@ export const gestorActividades = new GestorActividades();
 app.get('/', (req, res) => {
   res.send('¡Testing!');
 });
+
+
+app.use('/actividades', routerActividades);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
