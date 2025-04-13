b566c23979611d5d751043d81a1078856d6c1b6c
Fede-Coder <federiconieto04@gmail.com>
2025-04-13 19:29:45 -0300

    Fixed date in activities.

--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Activities/Card.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Activities/Card.jsx
@@ -9,15 +9,55 @@ export default function Card({
     fecha_fin,
     inscriptos,
 }) {
+    const fechaDesde = new Date(fecha_inicio);
+    const fechaFin = new Date(fecha_fin);
+
+    const { getHoraInicio, getMinInicio, getHoraFin, getMinFin } = {
+        getHoraInicio: ("0" + fechaDesde.getHours()).slice(-2),
+        getMinInicio: ("0" + fechaDesde.getMinutes()).slice(-2),
+        getHoraFin: ("0" + fechaFin.getHours()).slice(-2),
+        getMinFin: ("0" + fechaFin.getMinutes()).slice(-2),
+    };
+
     return (
         <>
-            <S.Box>
+            <div className="font-mono relative bg-[#384252] shadow-2xl mb-4 rounded-md font-semibold w-full text-center p-1 hover:bg-[#8cd19d] hover:p-3">
+                <div className="text-[#ffffdd] group text-shadow-lg/20">
+                    <p className="inline-block relative mr-50 bg-[#c0efd2] w-full rounded-2xl">
+                        {nombre}
+                    </p>
+                    <p
+                        className={`inline-block m-5 ${
+                            inscriptos <= cupos * 0.7
+                                ? "bg-[#d4f1db]"
+                                : "bg-red-300"
+                        } rounded-xl p-2`}
+                    >
+                        Cupos: {inscriptos}/{cupos}
+                    </p>
+                    <p className="inline-block bg-[#d4f1db] rounded-xl p-2">
+                        {getHoraInicio}:{getMinInicio} - {getHoraFin}:
+                        {getMinFin}
+                    </p>
+                    {inscriptos <= cupos * 0.7 ? (
+                        <p></p>
+                    ) : (
+                        <p className="p-1 group-hover:bg-[#384252] rounded-2xl m-2">
+                            Últimos lugares
+                        </p>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
+
+{
+    /* <S.Box>
                 <p>{nombre}</p>
                 <p>{cupos}</p>
                 <p>{fecha_inicio}</p>
                 <p>{fecha_fin}</p>
                 <p>{inscriptos}</p>
-            </S.Box>
-        </>
-    );
+            </S.Box> */
 }
