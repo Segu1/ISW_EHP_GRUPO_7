7f3fb773617b5178e894bb5d0457d3088fe4d9ff
Fede-Coder <federiconieto04@gmail.com>
2025-04-13 18:27:44 -0300

    Changed: fixed activities

--- EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Activities/Card.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/Frontend/ehp-us-inscribirse-en-actividad/src/Views/Activities/Card.jsx
@@ -1,19 +1,21 @@
 import { S } from "../../assets/css/Card.css";
 
+//"id":1,"nombre":"Tirolesa","cupos":10,"requiere_talla":false,"fecha_inicio":"2025-03-21T00:00:00.000Z"
+//{"id":1,"nombre":"Tirolesa","cupos":10,"requiere_talla":false,"fecha_inicio":"2025-03-21T00:00:00.000Z","fecha_fin":"2025-03-22T00:00:00.000Z","inscriptos":0}
 export default function Card({
     cupos,
     nombre,
-    fechaDesde,
-    fechaHasta,
+    fecha_inicio,
+    fecha_fin,
     inscriptos,
 }) {
     return (
         <>
             <S.Box>
-                <p>{cupos}</p>
                 <p>{nombre}</p>
-                <p>{fechaDesde}</p>
-                <p>{fechaHasta}</p>
+                <p>{cupos}</p>
+                <p>{fecha_inicio}</p>
+                <p>{fecha_fin}</p>
                 <p>{inscriptos}</p>
             </S.Box>
         </>
