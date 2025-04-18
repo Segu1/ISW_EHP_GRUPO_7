5965c4ae7470010a87fb0081333aefd6ff36b37c
Fede-Coder <federiconieto04@gmail.com>
2025-04-18 18:49:28 -0300

    Agregado boton Volver en navbar

--- EHP_PRODUCTO/EHP_Implementación/Código/EHP_Frontend/ehp-us-inscribirse-en-actividad/src/Components/Navbar.jsx
+++ EHP_PRODUCTO/EHP_Implementación/Código/EHP_Frontend/ehp-us-inscribirse-en-actividad/src/Components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { Button } from "antd";
+import { useNavigate } from "react-router";
 
 function Navbar(props){
+    const navigate = useNavigate();
     return(
         <>
         <div className="grid sticky z-5 top-0 text-shadow-lg items-center text-white w-full text-center p-3 bg-[#1F363D]">
             <p className="mt-1 block">EcoHarmony Park</p>
             <p> {props.site}</p>
+            <div className="absolute right-10">
+                {props.back &&
+                    <Button type="primary" onClick={() => navigate(-1)}>Volver</Button>
+                }
+            </div>
         </div>
         </>
     );
